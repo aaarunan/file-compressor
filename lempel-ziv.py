ba65f90992408f4ad2aa9c859f467d7bd215b21c
aaarunan <arunan2212@gmail.com>
2022-10-30 13:15:11 +0100

    temp solution

--- lempel-ziv.py
+++ lempel-ziv.py
@@ -1,31 +1,53 @@
+import re
+
+window_size = 32 * 1024
+
+
+def add_to_buffer(search_buffer, byte):
+    if len(search_buffer) >= window_size:
+        search_buffer.pop()
+    search_buffer.append(int.from_bytes(byte, byteorder="big"))
+
+
 def compress(file_in, file_out) -> None:
     search_buffer = bytearray()
     with open(file_in, "rb") as f_in, open(file_out, "wb") as f_out:
         while byte := f_in.read(1):
-            start = search_buffer.find(byte)
-            if start == -1:
-                search_buffer.append(int.from_bytes(byte, byteorder="big"))
-                f_out.write(byte)
+            add_to_buffer(search_buffer, byte)
+            f_out.write(byte)
+
+            if byte not in search_buffer:
                 continue
 
-            length = 0
+            start = 0
 
-            while byte := f_in.read(1):
-                temp = search_buffer.find(start + 1)
-                if temp == byte:
-                    length += 1
-                    return
-                search_buffer.append(int.from_bytes(byte, byteorder="big"))
-                f_out.write(byte)
-                break
+            temp_array = bytearray(byte)
+            matches = 0
+            start = 0
+            while byte1 := f_in.read(1):
+                temp_array.append(int.from_bytes(byte1, byteorder="big"))
+                for index, target in enumerate(search_buffer):
+                    if target != temp_array[0]:
+                        continue
+                    temp = find_length(index, temp_array, search_buffer)
+                    if temp > matches:
+                        matches = temp
+                        start = index
+            for byte1 in temp_array:
+                add_to_buffer(search_buffer, byte1.to_bytes(1, byteorder='big'))
 
-            f_out.write(byte)
-            f_out.write(start.to_bytes(1, byteorder="big"))
-            f_out.write(length.to_bytes(1, byteorder="big"))  # offset
-            #Funker ikke bra må reversere search_buffer 
-            # så jeg får det siste elemetet jeg la inn
-def decompress(file_in, file_out) -> None:
-    
+            print(matches)
+            if matches != 0:
+                f_out.write(start)
+                f_out.write(matches)
+
+
+def find_length(offset, temp_array, search_buffer):
+    matches = 0
+    for index in range(1, len(temp_array) - 1):
+        if index+offset < len(search_buffer) and temp_array[index] == search_buffer[index + offset]:
+            matches += 1
+    return matches
 
 
 def main():
